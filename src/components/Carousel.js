afa4f717f1932fb3a5ca52ab82f3b5f772111bb9
Ajinkya710 <ajinkyabuva7@gmail.com>
2023-02-21 10:27:22 +1100

    update-13

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../styles/Carousel.scss';
 import { Link } from 'react-router-dom';
+import  ISPAHlogocropped from '../images/posts/ISPAH-logo-cropped.jpg';
+import bubbles from '../images/posts/bubbles.jpg';
+import puddles from '../images/posts/puddles.jpg';
+import heightmorocco from '../images/posts/height-Morocco.jpg';
+import swedendc from '../images/posts/matiinu-ramadhan-Ovmog0nQGZ0-unsplash-1-scaled.jpg';
+import sunrise2021 from '../images/posts/Russia-data-collection-300x220.jpg';
+import placementstudents from '../images/posts/ceced5fd-c14c-439c-bc43-4919cdff84b3-1024x651.jpg';
+
 
 const slideWidth = 30;
 const _items = [
@@ -8,7 +16,7 @@ const _items = [
         post: {
             title: 'SUNRISE PUBLICATIONS September-October 2021',
             desc: 'We are happy to announce that SUNRISE have had two articles published recently. The SUNRISE team in Bangladesh, led by Mohammad Sorowar Hossain, in collaboration with the UOW coordinating team, have published an article titled, International Study of 24-hour Movement Behaviors of Early Years (SUNRISE)',
-            image: 'https://sunrise-study.com/wp-content/uploads/2021/11/ISPAH-logo-cropped.jpg',
+            image: ISPAHlogocropped,
             category: 'Publications',
             date: '01/11/2021',
             url: '/posts/SeptOct2021Update.html',
@@ -18,7 +26,7 @@ const _items = [
         post: {
             title: "SUNRISE NEWSLETTER SEPTEMBER/OCTOBER",
             desc: "The past two months brings with it a lot of good news from the SUNRISE team. On a very positive note, many countries are now seeing an ease in COVID-19 case numbers and as a result, restrictions on movements and activities are beginning to lift.",
-            image: 'https://sunrise-study.com/wp-content/uploads/2021/11/bubbles.jpg',
+            image: bubbles,
             category: 'Newsletter',
             date: '01/11/2021',
             url:'/posts/SeptOct2021Newsletter.html',
@@ -28,7 +36,7 @@ const _items = [
         post: {
             title: 'SUNRISE July/August 2021 Update',
             desc: 'Although it has been a challenging time to conduct research, we are happy to share that many of our SUNRISE teams have been lucky enough to continue with their data collection safely and have persevered through the ever-changing restrictions brought about due to the pandemic. We currently have Sweden, Papua New Guinea, and the United States of Americacollecting data for the Main Study and Nigeria collecting data for the Phase 3 Pilot Study.',
-            image: 'https://sunrise-study.com/wp-content/uploads/2021/09/puddles.jpg',
+            image: puddles,
             category: 'Newsletter',
             date: '09/09/2021',
             url:'/posts/JulyAug2021Update.html',
@@ -38,7 +46,7 @@ const _items = [
         post: {
             title: 'SUNRISE Morocco',
             desc: 'This month, we spoke with the team in Morocco about their experience participating in the SUNRISE study during the COVID-19 Pandemic.',
-            image: 'https://sunrise-study.com/wp-content/uploads/2021/09/height-Morocco.jpg',
+            image: heightmorocco,
             category: 'Newsletter',
             date: '09/09/2021',
             url:'/posts/SUNRISEMorocco.html',
@@ -48,7 +56,7 @@ const _items = [
         post: {
             title: 'Sweden Data Collection Update',
             desc: 'Sweden is the third country to commence data collection for the SUNRISE Main Study. The team reported that data collection is progressing nicely. Since their training in February 2021, they have visited 5 preschool centres for the SUNRISE Main Study and have successfully assessed over 115 children.',
-            image: 'https://sunrise-study.com/wp-content/uploads/2020/10/matiinu-ramadhan-Ovmog0nQGZ0-unsplash-1-scaled.jpg',
+            image: swedendc,
             category: 'Data collection',
             date: '01/06/2021',
             url:'/posts/SwedenDCUpdate.html',
@@ -58,7 +66,7 @@ const _items = [
         post: {
             title: 'SUNRISE 2021 Update',
             desc: 'Firstly, the SUNRISE Coordinating Centre team would like to extend our deepest sympathies, condolences, and our support to our friends within the SUNRISE India team and all nations effected by COVID-19. Our thoughts are with you',
-            image: 'https://sunrise-study.com/wp-content/uploads/2021/06/Russia-data-collection-300x220.png',
+            image: sunrise2021,
             category: 'Other Updates',
             date: '01/06/2021',
             url:'/posts/Sunrise2021Update.html',
@@ -68,7 +76,7 @@ const _items = [
         post: {
             title: 'SUNRISE Health & Society Placement Student Feedback',
             desc: 'For the past 13 weeks, we have been joined by University of Wollongong Health and Society placement students, Claudia Maddren and Hamad Aldughdugh. Below, read what Claudia and Hamad had to share regarding their experience. Thank you, Claudia and Hamad, for all your hard work!',
-            image: 'https://sunrise-study.com/wp-content/uploads/2020/12/ceced5fd-c14c-439c-bc43-4919cdff84b3-1024x651.jpg',
+            image: placementstudents,
             category: 'Other Updates',
             date: '01/12/2020',
             url:'/posts/SUNRISEPlacementStudentFeedback.html',
